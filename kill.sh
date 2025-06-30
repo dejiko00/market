00493d02b859901f1762a8e132411cd4abd82627
dejiko00 <63995081+dejiko00@users.noreply.github.com>
2025-06-29 19:03:55 -0500

    chore: moved dbs to repositories folder

--- kill.sh
+++ kill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CONTAINER_NAME=market-mssql
-docker container kill $(docker container ls -f name=$CONTAINER_NAME -q) &&
-docker container prune filter name=$CONTAINER_NAME
+# docker container ls -f name=$CONTAINER_NAME -q
+docker container rm -f $CONTAINER_NAME
 echo "~~~"
 echo "...They're gone. (´• ω •)"
